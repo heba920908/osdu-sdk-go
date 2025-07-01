ba88337d7bbc77a1293646dbcb220e1e9589d138
Arturo_Hernandez <Arturo_Hernandez@epam.com>
2025-07-01 12:20:36 -0600

    .

---
+++ pkg/models/file.go
@@ -0,0 +1,9 @@
+package models
+
+type FileGetSignedUrlResponse struct {
+	FileID   string `json:"FileID"`
+	Location struct {
+		SignedURL  string `json:"SignedURL"`
+		FileSource string `json:"FileSource"`
+	} `json:"Location"`
+}
