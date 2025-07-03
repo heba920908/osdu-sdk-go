66847087dfeeaa23b1cfffa2d8c1df7f6cf06846
Arturo_Hernandez <Arturo_Hernandez@epam.com>
2025-07-03 11:48:11 -0600

    Onboard dataset model v2

--- pkg/models/dataset.go
+++ pkg/models/dataset.go
@@ -1,7 +1,7 @@
 package models
 
 // OSDUDatasetResponse represents the structure of the OSDU dataset output response
-type DatasetResponse struct {
+type DatasetStorageInstructionsResponse struct {
 	ProviderKey     string                 `json:"providerKey"`
 	StorageLocation DatasetStorageLocation `json:"storageLocation"`
 }
@@ -9,7 +9,22 @@ type DatasetResponse struct {
 // StorageLocation represents the storage location details within the OSDU dataset response
 type DatasetStorageLocation struct {
 	SignedUrl  string `json:"signedUrl"`
-	FileSource string `json:"fileSource"`
+	FileSource string `json:"fileSource,omitempty"`
 	CreatedBy  string `json:"createdBy"`
 	ExpiryTime string `json:"expiryTime"`
+}
+
+type DatasetRetrievalRequest struct {
+	DatasetRegistryIds []string `json:"datasetRegistryIds"`
+}
+
+type DatasetRetrievalInstructionsResponse struct {
+	Datasets []Dataset `json:"datasets"`
+}
+
+// Dataset represents individual dataset details
+type Dataset struct {
+	DatasetRegistryId   string                 `json:"datasetRegistryId"`
+	RetrievalProperties DatasetStorageLocation `json:"retrievalProperties"`
+	ProviderKey         string                 `json:"providerKey"`
 }
